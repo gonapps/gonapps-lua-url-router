f38d689e5807a159447f086837aa1a3be4fbb7ef
Byeonggon Lee <gonny952@gmail.com>
2016-09-12 14:40:01 +0900

    Support request method

--- src/cubelua.lua
+++ src/cubelua.lua
@@ -4,18 +4,23 @@ local regex = require 'rex_pcre'
 local Path = {}
 Path.__index = Path
 
-function Path.new(pattern, callback)
+function Path.new(pattern, method, callback)
     local self = setmetatable({}, Path)
     self.regex = assert(regex.new(pattern))
+    self.method = method
     self.callback = callback
     return self
 end
 
-function Path:match(pattern)
-    local _
-    local result
-    _, _, result = self.regex:tfind(pattern)
-    return result
+function Path:match(pattern, method)
+    if self.method == method then
+        local _
+        local result
+         _, _, result = self.regex:tfind(pattern)
+        return result
+    else
+        return nil
+    end
 end
 
 _M.Router = {}
@@ -27,14 +32,14 @@ function _M.Router.new()
     return self
 end
 
-function _M.Router:setCallback(pattern, callback)
-    table.insert(self.paths, Path.new(pattern, callback))
+function _M.Router:setCallback(pattern, method, callback)
+    table.insert(self.paths, Path.new(pattern, method, callback))
 end
 
 function _M.Router:route(request)
     local result
     for _, path in ipairs(self.paths) do
-        result = path:match(request.pathInfo)
+        result = path:match(request.pathInfo, request.method)
         if result ~= nil then
             for key, value in pairs(result) do
                 if type(key) == 'string' then
--- src/test.lua
+++ src/test.lua
@@ -1,7 +1,8 @@
 local cubelua = require 'cubelua'
 local router = cubelua.Router:new()
-router:setCallback('/member/(?<number>\\d+)', function(request) print(request.parameter['number']) end)
+router:setCallback('/member/(?<number>\\d+)', 'GET', function(request) print(request.parameter['number']) end)
 local request = {}
 request.parameter = {}
 request.pathInfo = '/member/1254'
+request.method = 'GET'
 router:route(request)
