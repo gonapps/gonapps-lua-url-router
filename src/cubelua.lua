158e5ad258c1c22c5f1a36a579db28f847b4b385
Byeonggon Lee <gonny952@gmail.com>
2016-09-18 04:06:20 +0900

    Commit

--- src/cubelua.lua
+++ src/cubelua.lua
@@ -43,7 +43,7 @@ function _M.Router:route(request)
         if result ~= nil then
             for key, value in pairs(result) do
                 if type(key) == 'string' then
-                    request.parameters[key] = value
+                    request.parameters[_M.decodeURI(key)] = _M.decodeURI(value)
                 end
             end
             return path.callback(request)
