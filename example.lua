fcf2ab4f6b3995d2a77eef195067db21f8e5efd9
Byeonggon Lee <gonny952@gmail.com>
2016-09-25 02:03:23 +0900

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,7 +1,7 @@
 local router = require "tucube.http.router".new()
-router:setCallback('/member/(?<number>\\d+)', 'GET', function(request) print(request.parameters['number']) end)
+router:setCallback("/member/(?<number>\\d+)", "GET", function(request) print(request.parameters["number"]) end)
 local request = {}
 request.parameters = {}
-request.pathInfo = '/member/1254'
-request.method = 'GET'
+request.pathInfo = "/member/1254"
+request.method = "GET"
 router:route(request)
