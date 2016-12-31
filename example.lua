efd5e82b15b8d64c703a17f2f08d3c2005b7d3b8
Byeonggon Lee <gonny952@gmail.com>
2016-12-31 17:51:16 +0900

    Rename to gonapps.url.router

--- example.lua
+++ example.lua
@@ -1,4 +1,5 @@
-local router = require "tucube.http.router".new()
+local Router = require "gonapps.url.router"
+local router = Router.new()
 router:setCallback("/member/(?<number>\\d+)", "GET", function(request) print(request.parameters["number"]) end)
 local request = {}
 request.parameters = {}
